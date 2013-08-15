0d64b262c10207bc7ad060f637dc120e36d8dae3
bagiro44 <dmitriyremezov@MacBook-Air-Dmitriy.local>
2013-08-15 11:58:10 +0400

    PushToRefresh

--- News/ViewController.m
+++ News/ViewController.m
@@ -38,10 +38,11 @@
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void)
             {
-           
+                [self.tableView.pullToRefreshView stopAnimating];
             });
-    } position:(SVPullToRefreshPositionBottom)];
-    
+    } position:(SVPullToRefreshPositionTop)];
+[self.tableView triggerPullToRefresh];
+
 }
 
 
